11d5eb5d8f90155dfbcf48d13410b779e44aab9a
pratik6158 <amnshrma341@gmail.com>
2020-07-24 14:20:59 +0530

    !

--- JavaScript-009/Basic/assets/scripts/app.js
+++ JavaScript-009/Basic/assets/scripts/app.js
@@ -1,4 +1,17 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert('The Result is '+result);
+}
+
+add(10,20);
+
 currentResult = currentResult + 10;
 
-outputResult(currentResult,'');
+let calculationDescription = `${currentResult} + 10`;
+
+outputResult(add(23,45), calculationDescription);
+
+console.log("Hello World y");
