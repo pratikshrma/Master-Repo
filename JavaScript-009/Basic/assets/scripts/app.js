887b05243b5de7037b45a47bc9606644558ab8f4
pratik6158 <amnshrma341@gmail.com>
2020-07-25 09:44:03 +0530

    Java Script

--- JavaScript-009/Basic/assets/scripts/app.js
+++ JavaScript-009/Basic/assets/scripts/app.js
@@ -1,19 +1,64 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEnteries = [];
 
-function add(num1, num2) {
-  const calcDescription = `${currentResult} + ${userInput.value}`;
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
+}
 
-  currentResult = currentResult + parseInt(userInput.value);
-  // instead of doing parseint every time you could also just add another +
-  // operator in the end and that willl also do the same thing 
-  // like currentResult=curretResult + +userInput.value;
-  // thats it
-  outputResult(currentResult,calcDescription );
-  
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
+  const logEntry = {
+    operation: 'ADD',
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult
+  };
+  logEnteries.push(logEntry);
+
+  console.log(logEntry.operation);
+  console.log(logEnteries);
+}
+
+function sssssubtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput("-", initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
 }
 
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+
+// instead of doing parseint every time you could also just add another +
+// operator in the end and that willl also do the same thing
+// like currentResult=curretResult + +userInput.value;
+// thats it
+
 // so if you add () after the add it will call the add function imidieatly after finising
 // the sentencs but if you do not add the parenthesis in the end it will tell the browser engin that wait
 // when the condition is met then only exicute the function in our case it is click event
