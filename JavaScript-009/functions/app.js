6c5870e6affed7823cbaa466e0799fc0f7b7d95f
“Pratik” <amnshrma341@gmail.com>
2020-07-29 21:02:02 +0530

    java Script

---
+++ JavaScript-009/functions/app.js
@@ -0,0 +1,19 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame()
+{
+	console.log('Game is Starting Here');
+
+}
+
+const person=
+	{
+		greet: function(){
+			
+		}
+	};
+
+
+console.log(person);
+
+startGameBtn.addEventListener('click',startGame);
