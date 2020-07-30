645e761669cfc282f7c3c8f4c08224c84dc46b0f
“Pratik” <amnshrma341@gmail.com>
2020-07-30 21:32:12 +0530

    JavaScript

--- JavaScript-009/Rock_Paper_Scissor/app.js
+++ JavaScript-009/Rock_Paper_Scissor/app.js
@@ -75,3 +75,17 @@ startGameBtn.addEventListener('click', () => {
   alert(message);
   gameIsRunning = false;
 });
+
+
+// not Related With This Game
+
+const sumUp = (a, b, ...numbers) => {
+  let sum = 0;
+  for (const num of numbers) {
+    sum += num;
+  }
+  return sum;
+};
+
+console.log(sumUp(1, 5, 10, -3, 6, 10));
+console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
