ed049ff97c3830db8e5ba36d8d890ac59e45dc5c
“Pratik” <amnshrma341@gmail.com>
2020-08-24 09:44:35 +0530

    python

--- Python/regex/WebScrapping.py
+++ Python/regex/WebScrapping.py
@@ -1,10 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd 
 
 page=requests.get('https://forecast.weather.gov/MapClick.php?textField1=34.05&textField2=-118.25#.X0HZAXUza-8')
 soup=BeautifulSoup(page.content,'html.parser')
 week=soup.find(id="seven-day-forecast-body")
-# print(week)
-
 items=week.find_all(class_='tombstone-container')
-print(items[0])
+i=0
+day=[]
+desc=[]
+temp=[]
+# print(items[0])
+while i<8:
+    day.append(items[i].find(class_="period-name").get_text())
+    desc.append(items[i].find(class_="short-desc").get_text())
+    temp.append(items[i].find(class_="temp").get_text())
+    i+=1
+    # print("+++++++++")
+
+# now a for loop just for fun
+# period_name=[items.find(class_="period-name").get_text() for items in items]
+# this is just a for loop i just implemented a while loop
+ 
+df=pd.DataFrame(
+    {
+        "Day":day,
+        "Descreption":desc,
+        "Temperature":temp
+    }
+)
+
+# print(df)
+df.to_csv("First.csv")
