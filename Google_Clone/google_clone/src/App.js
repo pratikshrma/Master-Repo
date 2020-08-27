a6e2464a27d00c365e8961c65a937cd8d830b515
“Pratik” <amnshrma341@gmail.com>
2020-08-27 11:26:48 +0530

    yo

---
+++ Google_Clone/google_clone/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> hello lets make a googel clone</h1>
+{/* Home Page(The one with the search one) */}
+
+    </div>
+  );
+}
+
+export default App;
