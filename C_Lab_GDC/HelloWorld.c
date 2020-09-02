b821d06add41548e51848563a25b200aee7b3b48
“Pratik” <amnshrma341@gmail.com>
2020-09-02 15:33:48 +0530

    GDC

--- C_Lab_GDC/HelloWorld.c
+++ C_Lab_GDC/HelloWorld.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 int main()
 {
-	printf("Hello World");
+	printf("1st Line\n");
+	printf("2st Line\n");
+	printf("3st Line\n");
+	printf("4st Line\n");
+	printf("5st Line\n");
+	return 0;
 }
