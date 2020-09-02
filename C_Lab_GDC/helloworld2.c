86aaa9a2bd444ec3d4853c48a72c78d8b275ccdb
“Pratik” <amnshrma341@gmail.com>
2020-09-02 16:10:21 +0530

    GDC

---
+++ C_Lab_GDC/helloworld2.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main()
+{
+	printf("Hello My Name Is Pratik\n My roll no is 2024\n");
+}
---
+++ C_Lab_GDC/one_to_five.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int main(){
+	printf("one\ntwo\nthree\nfour\nfive\nsix\nseven\nEight\nnine\nten");
+}
