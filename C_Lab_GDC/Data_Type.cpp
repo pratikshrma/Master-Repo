feb7c900af1056cf31781d424d55eff64582f7e1
“Pratik” <amnshrma341@gmail.com>
2020-09-17 12:46:32 +0530

    Gdc

---
+++ C_Lab_GDC/Data_Type.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int i=1;
+	float j=1.24;
+	char k='P';
+	char a[15]="Hello World";
+	printf("SO the output is %s",a);
+	printf("\n%d\n",i);
+	printf("%f\n",j);
+	printf("%c\n",k);
+	return 0;
+}
