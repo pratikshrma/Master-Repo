b42d457ce2d6f82d8a5192afc520770d5a8125a9
“Pratik” <amnshrma341@gmail.com>
2020-10-08 16:07:58 +0530

    C

---
+++ C_Lab_GDC/Triangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int tr_size;//triangle size
+	printf("Enter the size of the triangle:- ");
+	scanf("%d",&tr_size);
+	int spaces=(tr_size);
+	for(int i=0;i<=tr_size;i++)
+	{
+		for(int k=spaces;k>=0;k--)
+		{
+		printf(" ");
+		}
+		spaces--;
+		int stars=(2*i)+1;
+		for(int j=0;j<stars;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
