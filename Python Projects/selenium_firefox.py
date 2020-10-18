27605cbbe582000977f8bc23284471d08b92cebd
pratik <amnshrma3411@gmail.com>
2020-10-18 09:13:03 +0530

    -

--- Python Projects/selenium_firefox.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver 
-DRIVER_SOURCE='/home/tank/Desktop/geckodriver'
-driver = webdriver.Firefox(executable_path=DRIVER_SOURCE) 
-  
-driver.get("https://google.com") 
-
-# its kinda not working
-
-# because these geckodriver aint compatible with firewfox esr only with regular firefox
--- Python Projects/selenium_opera.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver 
-DRIVER_SOURCE='/home/tank/Desktop/operadriver'
-driver = webdriver.Opera(executable_path=DRIVER_SOURCE) 
-  
-
-driver.get("https://www.google.com") 
-
