93c5e3861fab92fe2b2fc5990708d7ed984d8537
pratik <amnshrma3411@gmail.com>
2020-10-20 18:48:49 +0530

    Python

--- Python/Python_Projects/snake.py
+++ Python/Python_Projects/snake.py
@@ -141,8 +141,8 @@ def gameLoop():
 		if(game_over==False):
  				score=0
 		if x1==foodx and y1==foody:
-			foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-			foody = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+			foodx = (round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0)/2
+			foody = (round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0)/2
  			length_of_snake+=1
  			print("Yummy!!")
  		
