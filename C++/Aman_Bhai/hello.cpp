611fc364dd731240667489da6c9362292db5fbe9
pratik <amnshrma3411@gmail.com>
2020-10-21 13:30:03 +0530

    C++

---
+++ C++/Aman_Bhai/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int a;
+    cout<<"HEllo boi enter something"<<endl;
+    cin>>a;
+
+    std::cout<<"Hello World"<<std::endl;
+    return 0;
+}
