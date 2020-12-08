e7986da2873cbd280daa766b5a2dfeb72aeb495f
pratik <amnshrma3411@gmail.com>
2020-12-09 01:23:31 +0530

    C

--- C++/Aman_Bhai/BackTracking/intro.cpp
+++ C++/Aman_Bhai/BackTracking/intro.cpp
@@ -9,7 +9,8 @@ of the problem at any point of time
 //first if it is even possibe?
 //then how?
 /*
-1 0 1 0 1   so this is the mapyou can go in 1 not in 0 , 0's are walls 
+  so this is the mapyou can go in 1 not in 0 , 0's are walls 
+1 0 1 0 1 
 1 1 1 1 1
 0 1 0 1 0
 1 0 0 1 1
@@ -24,7 +25,68 @@ or iska output
 */
 #include <iostream>
 using namespace std;
+
+bool isSafe(int **arr,int x,int y,int n){
+	if(x<n && y<n && arr[x][y]==1){
+		return true;
+	}
+	return false;
+}
+
+bool ratInMaze(int** arr,int x,int y,int n,int** solArr){
+	//base condition
+	if((x==(n-1)) && (y==(n-1))){
+		solArr[x][y]=1;
+		return true;
+	}
+
+	if(isSafe(arr,x,y,n)){
+		solArr[x][y]=1;
+		if(ratInMaze(arr,x+1,y,n,solArr)){
+			return true;
+		}
+		if(ratInMaze(arr,x,y+1,n,solArr)){
+			return true;
+		}
+		//ab agar dono mei hi false ho gaya to tab karage ham backtrack
+		solArr[x][y]=0; //BACKTRACKING
+		return false;
+	}
+	return false;
+}
+
 int main(){
+	int n;
+	cin>>n;
+
+	int ** arr=new int *[n];
+	for(int i=0;i<n;i++){
+		arr[i]=new int[n];
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cin>>arr[i][j];
+		}
+	}
+	cout<<endl;
+
+	//solution arr
+	int ** solarr=new int *[n];
+	for(int i=0;i<n;i++){
+		solarr[i]=new int[n];
+		for(int j=0;j<n;j++){
+			solarr[i][j]=0;
+		}
+	}
+
+	if(ratInMaze(arr,0,0,n,solarr)){
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				cout<<arr[i][j]<<" ";
+			}
+			cout<<endl;
+		}	
+	}
 
 
     return 0;
